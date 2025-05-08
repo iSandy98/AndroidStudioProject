0225c5044789d369245e9bac69d16b1739de6f72
SSGeo11 <129564222+SSGeo11@users.noreply.github.com>
2025-05-08 22:17:38 +0900

    H2 style added

--- app/src/main/java/com/example/myfirstapplication/screens/TrackerScreen.kt
+++ app/src/main/java/com/example/myfirstapplication/screens/TrackerScreen.kt
@@ -1,7 +1,6 @@
 package com.example.myfirstapplication.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,13 +8,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
-import androidx.compose.material3.SliderColors
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,7 +21,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,6 +46,8 @@ fun TrackerScreen(){
         }
     }
 }
+val H2style = TextStyle(fontSize = 16.sp, fontFamily = robotoFamily,
+    fontWeight = FontWeight.Medium, color = Color(0xFF4B84F1))
 
 @Composable
 fun EmojiSlider(){
@@ -56,10 +56,7 @@ fun EmojiSlider(){
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Ваше настроение на сегодня", fontSize = 16.sp,
-            fontFamily = robotoFamily, fontWeight = FontWeight.Medium,
-            color = colorResource(R.color.blue_main)
-        )
+        Text(text = "Ваше настроение на сегодня", style = H2style)
         Spacer(Modifier.size(16.dp))
         Slider(
             value = sliderPosition,

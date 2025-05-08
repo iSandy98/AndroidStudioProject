4993d7de6d42cfdf33b0d03683ac057ece7a6512
SSGeo11 <129564222+SSGeo11@users.noreply.github.com>
2025-05-08 22:36:33 +0900

    Feedback added

--- app/src/main/java/com/example/myfirstapplication/screens/TrackerScreen.kt
+++ app/src/main/java/com/example/myfirstapplication/screens/TrackerScreen.kt
@@ -3,6 +3,7 @@ package com.example.myfirstapplication.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,22 +12,31 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderColors
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,10 +56,15 @@ fun TrackerScreen(){
         item{
             DreamSlider()
         }
+        item {
+            Feedback()
+        }
     }
 }
-val H2style = TextStyle(fontSize = 16.sp, fontFamily = robotoFamily,
-    fontWeight = FontWeight.Medium, color = Color(0xFF4B84F1))
+val H2style = TextStyle(
+    fontSize = 16.sp, fontFamily = robotoFamily,
+    fontWeight = FontWeight.Medium, color = Color(0xFF4B84F1)
+)
 
 @Composable
 fun EmojiSlider(){
@@ -198,7 +213,44 @@ fun DreamSlider() {
 
 @Composable
 fun Feedback() {
-
+    var feedbackText by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Отклик лечащему врачу", style = H2style)
+        Spacer(Modifier.size(10.dp))
+        Text("Пожеланию можете оставить сообщение Вашему лечащему врачу.", style = H2style)
+        Spacer(Modifier.size(16.dp))
+        TextField(value = feedbackText, onValueChange = { feedbackText = it},
+            modifier = Modifier.fillMaxWidth().height(56.dp),
+            shape = RoundedCornerShape(8.dp),
+            maxLines = 1,
+            textStyle = TextStyle(fontSize = 14.sp, fontFamily = robotoFamily, fontWeight = FontWeight.Normal),
+            placeholder = { Text(text = "Начни писать здесь", fontSize = 14.sp, color = colorResource(R.color.gray), fontFamily = robotoFamily) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            )
+        )
+        Spacer(Modifier.size(12.dp))
+        //Календарь
+        Spacer(Modifier.size(30.dp))
+        Button(
+            onClick = {},
+            modifier = Modifier.height(48.dp),
+            colors = ButtonColors(
+                containerColor = colorResource(R.color.blue_main),
+                contentColor = colorResource(R.color.white),
+                disabledContainerColor = colorResource(R.color.blue_disable),
+                disabledContentColor = colorResource(R.color.gray)
+            )
+        ) {
+            Text(text = "Сохранить")
+        }
+    }
 }
 
 
